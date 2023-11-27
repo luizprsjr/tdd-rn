c6a3a5c6ae4b27b9942df8c20e8620fe669bfb8e
Luiz Pires <lpiresjunior@gmail.com>
2023-11-27 00:42:32 -0300

    feat: create home screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,40 @@
 import React from 'react';
-import {View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import {Colors} from '../constants';
+import moment from 'moment';
 
 export function HomeScreen() {
-  return <View testID="home-screen" />;
+  const now = moment(new Date());
+  return (
+    <LinearGradient
+      testID="home-screen"
+      colors={[Colors.LIGHT_GRAY, Colors.DARK_GRAY]}
+      style={styles.container}>
+      <View style={styles.title}>
+        <Text style={styles.date}>{now.format('MMM DD, YYYY')}</Text>
+        <Text style={styles.day}>{now.format('dddd')}</Text>
+      </View>
+    </LinearGradient>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 40,
+    alignContent: 'space-between',
+    justifyContent: 'space-evenly',
+  },
+  title: {
+    justifyContent: 'flex-end',
+  },
+  date: {
+    color: Colors.GRAY,
+    fontSize: 16,
+  },
+  day: {
+    color: Colors.WHITE,
+    fontSize: 24,
+  },
+});
