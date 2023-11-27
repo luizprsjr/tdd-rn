7e44f13848248799d87113dfde67451ac6090c70
Luiz Pires <lpiresjunior@gmail.com>
2023-11-27 00:42:47 -0300

    test: implement date tests

--- src/screens/__tests__/HomeScreen.test.tsx
+++ src/screens/__tests__/HomeScreen.test.tsx
@@ -7,4 +7,25 @@ describe('HomeScreen', () => {
     render(<HomeScreen />);
     screen.getByTestId('home-screen');
   });
+
+  describe('Title section', () => {
+    beforeEach(() => {
+      jest.useFakeTimers();
+      jest.setSystemTime(946684800000);
+    });
+
+    afterAll(() => {
+      jest.useRealTimers();
+    });
+
+    it('should contain current date', () => {
+      render(<HomeScreen />);
+      expect(screen.getByText('Jan 01, 2000')).toBeDefined();
+    });
+
+    it('should contain current day', () => {
+      render(<HomeScreen />);
+      expect(screen.getByText('Saturday')).toBeDefined();
+    });
+  });
 });
