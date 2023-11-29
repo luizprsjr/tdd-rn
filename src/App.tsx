926d7f087004f1625d815be2c08800401a917117
Luiz Pires <lpiresjunior@gmail.com>
2023-11-29 18:18:15 -0300

    feat: add status bar style

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import {AppNavigator} from './screens';
 import {Provider} from 'react-redux';
 import store from './store';
+import {StatusBar} from 'react-native';
 
 export function App() {
   return (
     <Provider store={store}>
+      <StatusBar barStyle="light-content" />
       <AppNavigator />
     </Provider>
   );
