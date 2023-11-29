f8496db0a1608d91cb9e13da747bcd6de744604a
Luiz Pires <lpiresjunior@gmail.com>
2023-11-29 00:51:38 -0300

    feat: get current location

--- src/components/WeatherCurrent.tsx
+++ src/components/WeatherCurrent.tsx
@@ -1,6 +1,42 @@
-import React from 'react';
-import {View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import React, {useCallback, useState} from 'react';
+import {LocationService} from '../services/LocationService';
+import {Button} from './Button';
+import {StyleSheet} from 'react-native';
+import {Colors} from '../constants';
 
 export function WeatherCurrent() {
-  return <View testID="weather-current" />;
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigation = useNavigation();
+
+  const handleFetchWeather = useCallback(async () => {
+    setError(false);
+    setLoading(true);
+    try {
+      const position = await LocationService.getCurrentPosition();
+      navigation.navigate('Weather', position);
+    } catch (e) {
+      setError(true);
+    }
+    setLoading(false);
+  }, [navigation]);
+
+  return (
+    <Button
+      testID="weather-current"
+      label="weather at my position"
+      onPress={handleFetchWeather}
+      loading={loading}
+      style={error && styles.error}
+    />
+  );
 }
+
+const styles = StyleSheet.create({
+  error: {
+    borderColor: Colors.ERROR,
+    borderWidth: 1,
+    borderRadius: 10,
+  },
+});
--- src/components/__tests__/WeatherCurrent.test.tsx
+++ src/components/__tests__/WeatherCurrent.test.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
-import {render, screen} from '@testing-library/react-native';
+import {
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from '@testing-library/react-native';
 import {WeatherCurrent} from '../WeatherCurrent';
+import {useNavigation} from '@react-navigation/native';
+import {LocationService} from '../../services/LocationService';
+import {act} from 'react-test-renderer';
+import {Colors} from '../../constants';
+
+jest.mock('@react-navigation/native', () => {
+  return {
+    ...jest.requireActual<object>('@react-navigation/native'),
+    useNavigation: jest.fn().mockReturnValue({navigate: jest.fn()}),
+  };
+});
 
 describe('WeatherCurrent', () => {
   it('should render correctly', () => {
@@ -8,7 +24,113 @@ describe('WeatherCurrent', () => {
     expect(screen.getByTestId('weather-current')).toBeDefined();
   });
 
-  it('should navigate to weather screen with location', () => {
-    throw new Error('Test not implemented');
+  it('should render label', () => {
+    render(<WeatherCurrent />);
+    screen.getByText(/weather at my position/i);
+  });
+
+  it('should navigate to weather screen with location', async () => {
+    const mockNavigate = jest.fn();
+    (useNavigation as jest.Mock).mockReturnValueOnce({navigate: mockNavigate});
+    render(<WeatherCurrent />);
+    const button = screen.getByTestId('weather-current');
+    fireEvent.press(button);
+
+    await waitFor(() => {
+      expect(mockNavigate).toHaveBeenCalledWith('Weather', {
+        latitude: 0,
+        longitude: 0,
+      });
+    });
+  });
+
+  describe('Loader', () => {
+    it('should be renderer when position is being fetched', async () => {
+      const mockNavigate = jest.fn();
+      (useNavigation as jest.Mock).mockReturnValueOnce({
+        navigate: mockNavigate,
+      });
+
+      let mockResolve!: (position: {
+        latitude: number;
+        longitude: number;
+      }) => void;
+
+      jest.spyOn(LocationService, 'getCurrentPosition').mockImplementationOnce(
+        () =>
+          new Promise(resolve => {
+            mockResolve = resolve;
+          }),
+      );
+
+      const {getByTestId} = render(<WeatherCurrent />);
+
+      fireEvent.press(getByTestId('weather-current'));
+
+      await waitFor(() => {
+        expect(getByTestId('button-loading')).toBeDefined();
+      });
+
+      await act(async () => {
+        await mockResolve({latitude: 0, longitude: 0});
+      });
+    });
+
+    it('should not be rendered when position been fetched', async () => {
+      render(<WeatherCurrent />);
+      const button = screen.getByTestId('weather-current');
+      fireEvent.press(button);
+      await waitFor(() => {
+        expect(screen.queryByTestId('button-loading')).toBeNull();
+      });
+    });
+
+    it('should not be rendered when fetching position has failed', async () => {
+      jest
+        .spyOn(LocationService, 'getCurrentPosition')
+        .mockRejectedValue(new Error());
+
+      render(<WeatherCurrent />);
+      const button = screen.getByTestId('weather-current');
+      fireEvent.press(button);
+      await waitFor(() => {
+        expect(screen.queryByTestId('button-loading')).toBeNull();
+      });
+    });
+  });
+
+  describe('Error', () => {
+    it('should be displayed after fetching position has failed', async () => {
+      jest
+        .spyOn(LocationService, 'getCurrentPosition')
+        .mockRejectedValueOnce(new Error());
+
+      render(<WeatherCurrent />);
+      const button = screen.getByTestId('weather-current');
+      fireEvent.press(button);
+
+      await waitFor(() => {
+        expect(button).toHaveStyle({
+          borderColor: Colors.ERROR,
+        });
+      });
+    });
+
+    it('should be removed after fetching position again', async () => {
+      jest
+        .spyOn(LocationService, 'getCurrentPosition')
+        .mockRejectedValueOnce(new Error());
+
+      render(<WeatherCurrent />);
+      const button = screen.getByTestId('weather-current');
+      fireEvent.press(button);
+
+      await waitFor(() => {
+        fireEvent.press(button);
+        expect(button).not.toHaveStyle({
+          borderColor: Colors.ERROR,
+        });
+      });
+    });
   });
 });
