4052fe0c3fe44a93d2d5302ab74b1c986ba4986a
Luiz Pires <lpiresjunior@gmail.com>
2023-11-29 14:51:54 -0300

    feat: implement coordinates form

--- src/components/WeatherCoordinates.tsx
+++ src/components/WeatherCoordinates.tsx
@@ -1,6 +1,102 @@
+import {useNavigation} from '@react-navigation/native';
 import React from 'react';
-import {View} from 'react-native';
+import {Controller, useForm} from 'react-hook-form';
+import {StyleSheet, Text, View} from 'react-native';
+import {Button} from './Button';
+import {TextInput} from 'react-native-gesture-handler';
+import {Colors} from '../constants';
+import Yup from 'json-schema-yup-transformer/dist/yup/addMethods';
+import {yupResolver} from '@hookform/resolvers/yup';
+
+type FormValues = {
+  latitude: number;
+  longitude: number;
+};
 
 export function WeatherCoordinates() {
-  return <View testID="weather-coordinates" />;
+  const navigation = useNavigation();
+
+  const form = useForm<FormValues>({
+    resolver: yupResolver(validationSchema),
+    defaultValues,
+    mode: 'onChange',
+  });
+
+  const handleSubmit = form.handleSubmit(values => {
+    navigation.navigate('Weather', values);
+  });
+
+  return (
+    <View testID="weather-coordinates">
+      <View style={styles.inputs}>
+        <Controller
+          control={form.control}
+          name="latitude"
+          render={({field, ...props}) => (
+            <TextInput
+              {...props}
+              testID="weather-coordinates-latitude"
+              onChangeText={field.onChange}
+              style={styles.input}
+              placeholder="Lat"
+              placeholderTextColor={Colors.GRAY}
+            />
+          )}
+        />
+        {form.formState.errors.latitude && (
+          <Text style={styles.error}>Latitude must be a valid number</Text>
+        )}
+        <Controller
+          control={form.control}
+          name="longitude"
+          render={({field, ...props}) => (
+            <TextInput
+              {...props}
+              testID="weather-coordinates-longitude"
+              onChangeText={field.onChange}
+              style={styles.input}
+              placeholder="Lon"
+              placeholderTextColor={Colors.GRAY}
+            />
+          )}
+        />
+        {form.formState.errors.longitude && (
+          <Text style={styles.error}>Longitude must be a valid number</Text>
+        )}
+      </View>
+
+      <Button onPress={handleSubmit} label="find" />
+    </View>
+  );
 }
+
+const defaultValues: FormValues = {
+  latitude: 0,
+  longitude: 0,
+};
+
+const validationSchema = Yup.object().shape({
+  latitude: Yup.number().required().min(-90).max(90),
+  longitude: Yup.number().required().min(-180).max(180),
+});
+
+const styles = StyleSheet.create({
+  inputs: {
+    flexDirection: 'column',
+    marginBottom: 15,
+  },
+  input: {
+    backgroundColor: Colors.TRANSPARENT,
+    margin: 5,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: Colors.GRAY,
+    paddingHorizontal: 15,
+    paddingVertical: 8,
+    color: Colors.WHITE,
+  },
+  error: {
+    marginHorizontal: 5,
+    color: Colors.ERROR,
+  },
+});
--- src/components/__tests__/WeatherCoordinates.test.tsx
+++ src/components/__tests__/WeatherCoordinates.test.tsx
@@ -1,10 +1,176 @@
-import {render, screen} from '@testing-library/react-native';
+import {
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from '@testing-library/react-native';
 import React from 'react';
 import {WeatherCoordinates} from '../WeatherCoordinates';
+import {useNavigation} from '@react-navigation/native';
+
+jest.mock('@react-navigation/native', () => {
+  return {
+    ...jest.requireActual<object>('@react-navigation/native'),
+    useNavigation: jest.fn().mockReturnValue({navigate: jest.fn()}),
+  };
+});
 
 describe('WeatherCurrent', () => {
   it('should render correctly', () => {
     render(<WeatherCoordinates />);
     expect(screen.getByTestId('weather-coordinates')).toBeDefined();
+  });
+
+  it('should navigate to weather screen with given coordinates when valid form is submit', async () => {
+    const mockNavigate = jest.fn();
+    (useNavigation as jest.Mock).mockReturnValueOnce({
+      navigate: mockNavigate,
+    });
+
+    render(<WeatherCoordinates />);
+
+    const fields = {
+      latitude: screen.getByTestId('weather-coordinates-latitude'),
+      longitude: screen.getByTestId('weather-coordinates-longitude'),
+    };
+
+    fireEvent.changeText(fields.latitude, 0);
+    fireEvent.changeText(fields.longitude, 0);
+
+    const button = screen.getByTestId('button');
+    fireEvent.press(button);
+
+    await waitFor(() => {
+      expect(mockNavigate).toHaveBeenCalledWith('Weather', {
+        latitude: 0,
+        longitude: 0,
+      });
+    });
+  });
+
+  describe('Latitude Field', () => {
+    it('should not show an error when value is the lowest range value', async () => {
+      render(<WeatherCoordinates />);
+      const field = screen.getByTestId('weather-coordinates-latitude');
+      fireEvent.changeText(field, '-90');
+
+      await waitFor(() => {
+        expect(
+          screen.queryByText(/Latitude must be a valid number/i),
+        ).not.toBeOnTheScreen();
+      });
+    });
+
+    it('should not show an error when value is the highest range value', async () => {
+      render(<WeatherCoordinates />);
+      const field = screen.getByTestId('weather-coordinates-latitude');
+      fireEvent.changeText(field, '90');
+
+      await waitFor(() => {
+        expect(
+          screen.queryByText(/Latitude must be a valid number/i),
+        ).not.toBeOnTheScreen();
+      });
+    });
+
+    it('should show an error when value is lower than the lowest range value', async () => {
+      render(<WeatherCoordinates />);
+      const field = screen.getByTestId('weather-coordinates-latitude');
+      fireEvent.changeText(field, '-91');
+
+      await waitFor(() => {
+        expect(
+          screen.queryByText(/Latitude must be a valid number/i),
+        ).toBeOnTheScreen();
+      });
+    });
+
+    it('should show an error when value is higher than the highest value', async () => {
+      render(<WeatherCoordinates />);
+      const field = screen.getByTestId('weather-coordinates-latitude');
+      fireEvent.changeText(field, '91');
+
+      await waitFor(() => {
+        expect(
+          screen.queryByText(/Latitude must be a valid number/i),
+        ).toBeOnTheScreen();
+      });
+    });
+
+    it('should show an error when value is not a number', async () => {
+      render(<WeatherCoordinates />);
+
+      const field = screen.getByTestId('weather-coordinates-latitude');
+      fireEvent.changeText(field, 'x');
+
+      await waitFor(() => {
+        expect(
+          screen.queryByText(/Latitude must be a valid number/i),
+        ).toBeOnTheScreen();
+      });
+    });
+  });
+
+  describe('Longitude Field', () => {
+    it('should not show an error when value is the lowest range value', async () => {
+      render(<WeatherCoordinates />);
+      const field = screen.getByTestId('weather-coordinates-longitude');
+      fireEvent.changeText(field, '-180');
+
+      await waitFor(() => {
+        expect(
+          screen.queryByText(/Longitude must be a valid number/i),
+        ).not.toBeOnTheScreen();
+      });
+    });
+
+    it('should not show an error when value is the highest range value', async () => {
+      render(<WeatherCoordinates />);
+      const field = screen.getByTestId('weather-coordinates-longitude');
+      fireEvent.changeText(field, '180');
+
+      await waitFor(() => {
+        expect(
+          screen.queryByText(/Longitude must be a valid number/i),
+        ).not.toBeOnTheScreen();
+      });
+    });
+
+    it('should show an error when value is lower than the lowest range value', async () => {
+      render(<WeatherCoordinates />);
+      const field = screen.getByTestId('weather-coordinates-longitude');
+      fireEvent.changeText(field, '-181');
+
+      await waitFor(() => {
+        expect(
+          screen.queryByText(/Longitude must be a valid number/i),
+        ).toBeOnTheScreen();
+      });
+    });
+
+    it('should show an error when value is higher than the highest value', async () => {
+      render(<WeatherCoordinates />);
+      const field = screen.getByTestId('weather-coordinates-longitude');
+      fireEvent.changeText(field, '181');
+
+      await waitFor(() => {
+        expect(
+          screen.queryByText(/Longitude must be a valid number/i),
+        ).toBeOnTheScreen();
+      });
+    });
+
+    it('should show an error when value is not a number', async () => {
+      render(<WeatherCoordinates />);
+
+      const field = screen.getByTestId('weather-coordinates-longitude');
+      fireEvent.changeText(field, 'x');
+
+      await waitFor(() => {
+        expect(
+          screen.queryByText(/Longitude must be a valid number/i),
+        ).toBeOnTheScreen();
+      });
+    });
   });
 });
